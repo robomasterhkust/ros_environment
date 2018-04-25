48228ef6579bdf74c20b4e1eecb5adce200821b7
Beck <pshfls@gmail.com>
2018-04-25 16:26:46 +0800

    working on CAN publishing

---
+++ 0_driver/canmaster/src/canmaster_node.cpp
@@ -0,0 +1,41 @@
+#include <ros/ros.h>
+#include <can_msgs/Frame.h>
+#include <geometry_msgs/Twist.h>
+
+#define CAN_NVIDIA_TX2_BOARD_ID 0x103
+
+ros::Publisher can_publisher;
+ros::Subscriber cmd_vel_subscriber;
+
+void cmd_cb(const geometry_msgs::Twist &t){
+	static can_msgs::Frame f;
+
+	ROS_INFO("Received cmd_vel vx=%f vy=%f vw=%f",t.linear.x,t.linear.y,t.angular.z);
+	f.id = CAN_NVIDIA_TX2_BOARD_ID;
+	f.dlc = (16 / 8) * 3;
+
+	int16_t vx = (int16_t) (t.linear.x * 1000); // convert to mm/s
+	f.data[0] = (uint8_t) (vx >> 8) & 0xff;
+	f.data[1] = (uint8_t) vx & 0xff;
+	
+	int16_t vy = (int16_t) (t.linear.y * 1000); // convert to mm/s
+	f.data[2] = (uint8_t) (vy >> 8) & 0xff;
+	f.data[3] = (uint8_t) vy & 0xff;
+
+	int16_t vw = (int16_t) t.angular.z;
+	f.data[4] = (uint8_t) (vw >> 8) & 0xff;
+	f.data[5] = (uint8_t) vw & 0xff;
+
+	can_publisher.publish(f);
+}
+
+int main(int argc, char* argv[]){
+    ros::init(argc,argv,"canmaster_node");
+    ros::NodeHandle nh("~");
+
+	can_publisher = nh.advertise<can_msgs::Frame>("/sent_messages",10);
+	cmd_vel_subscriber = nh.subscribe("/cmd_vel",10,cmd_cb);
+	
+	ROS_INFO("CANMaster node started");
+    ros::spin();
+}
