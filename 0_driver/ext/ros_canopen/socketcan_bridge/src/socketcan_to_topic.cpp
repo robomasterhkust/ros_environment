d1c422777850931fe52bb3725cc3a528ce610f8d
Beck <pshfls@gmail.com>
2018-04-26 15:48:35 +0800

    modify ros canopen a little bit

--- 0_driver/ext/ros_canopen/socketcan_bridge/src/socketcan_to_topic.cpp
+++ 0_driver/ext/ros_canopen/socketcan_bridge/src/socketcan_to_topic.cpp
@@ -31,97 +31,90 @@
 #include <string>
 
 namespace can {
-template<> can::FrameFilter::Ptr tofilter(const XmlRpc::XmlRpcValue  &ct) {
-  XmlRpc::XmlRpcValue t(ct);
-  try{ // to read as integer
-      uint32_t id = static_cast<int>(t);
-      return tofilter(id);
-  }
-  catch(...){ // read as string
-      return  tofilter(static_cast<std::string>(t));
-  }
-}
-}
-
-namespace socketcan_bridge
-{
-  SocketCANToTopic::SocketCANToTopic(ros::NodeHandle* nh, ros::NodeHandle* nh_param,
-      boost::shared_ptr<can::DriverInterface> driver)
-    {
-      can_topic_ = nh->advertise<can_msgs::Frame>("received_messages", 10);
-      driver_ = driver;
-    };
-
-  void SocketCANToTopic::setup()
-    {
-      // register handler for frames and state changes.
-      frame_listener_ = driver_->createMsgListener(
-              can::CommInterface::FrameDelegate(this, &SocketCANToTopic::frameCallback));
-
-      state_listener_ = driver_->createStateListener(
-              can::StateInterface::StateDelegate(this, &SocketCANToTopic::stateCallback));
-    };
-
-  void SocketCANToTopic::setup(const can::FilteredFrameListener::FilterVector &filters){
-    frame_listener_.reset(new can::FilteredFrameListener(driver_,
-                                                         can::CommInterface::FrameDelegate(this, &SocketCANToTopic::frameCallback),
-                                                         filters));
-
-    state_listener_ = driver_->createStateListener(
-            can::StateInterface::StateDelegate(this, &SocketCANToTopic::stateCallback));
-  }
-
-  void SocketCANToTopic::setup(XmlRpc::XmlRpcValue filters) {
-      setup(can::tofilters(filters));
-  }
-  void SocketCANToTopic::setup(ros::NodeHandle nh) {
-       XmlRpc::XmlRpcValue filters;
-       if(nh.getParam("can_ids", filters)) return setup(filters);
-       return setup();
-  }
-
-
-  void SocketCANToTopic::frameCallback(const can::Frame& f)
-    {
-      // ROS_DEBUG("Message came in: %s", can::tostring(f, true).c_str());
-      if (!f.isValid())
-      {
-        ROS_ERROR("Invalid frame from SocketCAN: id: %#04x, length: %d, is_extended: %d, is_error: %d, is_rtr: %d",
-                  f.id, f.dlc, f.is_extended, f.is_error, f.is_rtr);
-        return;
-      }
-      else
-      {
-        if (f.is_error)
-        {
-          // can::tostring cannot be used for dlc > 8 frames. It causes an crash
-          // due to usage of boost::array for the data array. The should always work.
-          ROS_WARN("Received frame is error: %s", can::tostring(f, true).c_str());
+    template<>
+    can::FrameFilter::Ptr tofilter(const XmlRpc::XmlRpcValue &ct) {
+        XmlRpc::XmlRpcValue t(ct);
+        try { // to read as integer
+            uint32_t id = static_cast<int>(t);
+            return tofilter(id);
         }
-      }
+        catch (...) { // read as string
+            return tofilter(static_cast<std::string>(t));
+        }
+    }
+}
 
-      can_msgs::Frame msg;
-      // converts the can::Frame (socketcan.h) to can_msgs::Frame (ROS msg)
-      convertSocketCANToMessage(f, msg);
+namespace socketcan_bridge {
+    SocketCANToTopic::SocketCANToTopic(ros::NodeHandle *nh, ros::NodeHandle *nh_param,
+                                       boost::shared_ptr <can::DriverInterface> driver) {
+        std::string can_device;
+        nh_param->getParam("can_device", can_device);
+        can_topic_ = nh->advertise<can_msgs::Frame>(can_device + "_received_messages_raw", 10);
+        driver_ = driver;
+    };
 
-      msg.header.frame_id = "";  // empty frame is the de-facto standard for no frame.
-      msg.header.stamp = ros::Time::now();
+    void SocketCANToTopic::setup() {
+        // register handler for frames and state changes.
+        frame_listener_ = driver_->createMsgListener(
+                can::CommInterface::FrameDelegate(this, &SocketCANToTopic::frameCallback));
 
-      can_topic_.publish(msg);
+        state_listener_ = driver_->createStateListener(
+                can::StateInterface::StateDelegate(this, &SocketCANToTopic::stateCallback));
+    };
+
+    void SocketCANToTopic::setup(const can::FilteredFrameListener::FilterVector &filters) {
+        frame_listener_.reset(new can::FilteredFrameListener(driver_,
+                                                             can::CommInterface::FrameDelegate(this,
+                                                                                               &SocketCANToTopic::frameCallback),
+                                                             filters));
+
+        state_listener_ = driver_->createStateListener(
+                can::StateInterface::StateDelegate(this, &SocketCANToTopic::stateCallback));
+    }
+
+    void SocketCANToTopic::setup(XmlRpc::XmlRpcValue filters) {
+        setup(can::tofilters(filters));
+    }
+
+    void SocketCANToTopic::setup(ros::NodeHandle nh) {
+        XmlRpc::XmlRpcValue filters;
+        if (nh.getParam("can_ids", filters)) return setup(filters);
+        return setup();
+    }
+
+
+    void SocketCANToTopic::frameCallback(const can::Frame &f) {
+        // ROS_DEBUG("Message came in: %s", can::tostring(f, true).c_str());
+        if (!f.isValid()) {
+            ROS_ERROR("Invalid frame from SocketCAN: id: %#04x, length: %d, is_extended: %d, is_error: %d, is_rtr: %d",
+                      f.id, f.dlc, f.is_extended, f.is_error, f.is_rtr);
+            return;
+        } else {
+            if (f.is_error) {
+                // can::tostring cannot be used for dlc > 8 frames. It causes an crash
+                // due to usage of boost::array for the data array. The should always work.
+                ROS_WARN("Received frame is error: %s", can::tostring(f, true).c_str());
+            }
+        }
+
+        can_msgs::Frame msg;
+        // converts the can::Frame (socketcan.h) to can_msgs::Frame (ROS msg)
+        convertSocketCANToMessage(f, msg);
+
+        msg.header.frame_id = "";  // empty frame is the de-facto standard for no frame.
+        msg.header.stamp = ros::Time::now();
+
+        can_topic_.publish(msg);
     };
 
 
-  void SocketCANToTopic::stateCallback(const can::State & s)
-    {
-      std::string err;
-      driver_->translateError(s.internal_error, err);
-      if (!s.internal_error)
-      {
-        ROS_INFO("State: %s, asio: %s", err.c_str(), s.error_code.message().c_str());
-      }
-      else
-      {
-        ROS_ERROR("Error: %s, asio: %s", err.c_str(), s.error_code.message().c_str());
-      }
+    void SocketCANToTopic::stateCallback(const can::State &s) {
+        std::string err;
+        driver_->translateError(s.internal_error, err);
+        if (!s.internal_error) {
+            ROS_INFO("State: %s, asio: %s", err.c_str(), s.error_code.message().c_str());
+        } else {
+            ROS_ERROR("Error: %s, asio: %s", err.c_str(), s.error_code.message().c_str());
+        }
     };
 };  // namespace socketcan_bridge
