6338db372a05fe23ff205dec2785b3ae37d9b254
Yang Shaohui <syangav@gmail.com>
2018-07-04 19:05:27 +0800

    usb to can rx&tx finished

--- 0_driver/usb_can/src/usbCan_node.cpp
+++ 0_driver/usb_can/src/usbCan_node.cpp
@@ -9,10 +9,14 @@
 #include <thread>
 #include "SerialCan.hpp"
 #include "can_msgs/Frame.h"
+#include <geometry_msgs/Twist.h>
 #include "ros/ros.h"
 
 ros::NodeHandle *nh;
 SerialCan *comObj = NULL;
+
+#define CAN_NVIDIA_TX2_BOARD_ID 0x103
+
 
 void subCB(const can_msgs::Frame &msg)
 {
@@ -23,12 +27,45 @@ void subCB(const can_msgs::Frame &msg)
                        msg.data.begin());
 }
 
+void cmd_cb(const geometry_msgs::Twist &t){
+// void cmd_cb(const geometry_msgs::Vector3 &t){
+	static can_msgs::Frame f;
+
+	// ROS_INFO("Received cmd_vel py=%f vy=%f vw=%f",t.linear.x,t.linear.y,t.angular.z);
+	//f.header.frame_id="0";
+	f.header.stamp = ros::Time::now();
+
+	f.id = CAN_NVIDIA_TX2_BOARD_ID;
+	f.dlc = (16 / 8) * 4;
+
+	int16_t py = (int16_t) (t.linear.y  * 1000); // convert to mm/s
+	int16_t pz = (int16_t) (t.linear.z  * 1000); // convert to mm/s
+    int16_t vy = (int16_t) (t.angular.y * 1000); // pitch, rotate by Y axis
+    int16_t vz = (int16_t) (t.angular.z * 1000); // yaw,   rotate by Z axis
+	// int16_t py = (int16_t) (t.z * 100000); // convert to mm/s
+	// int16_t vy = (int16_t) (t.x * 100000); // convert to mm/s
+	// int16_t vw = (int16_t) (t.y * 100000); // convert to mm/s
+	f.data[1] = (uint8_t) (py >> 8) & 0xff;
+	f.data[0] = (uint8_t) py & 0xff;
+
+	f.data[3] = (uint8_t) (pz >> 8) & 0xff;
+	f.data[2] = (uint8_t) pz & 0xff;
+
+	f.data[5] = (uint8_t) (vy >> 8) & 0xff;
+	f.data[4] = (uint8_t) vy & 0xff;
+
+	f.data[7] = (uint8_t) (vz >> 8) & 0xff;
+	f.data[6] = (uint8_t) vz & 0xff;
+
+	subCB(f);
+}
+
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "canusb0");
+    ros::init(argc, argv, "canusb1");
     nh = new ros::NodeHandle("~");
     ros::Publisher pub = nh->advertise<can_msgs::Frame>("canRx", 20);
-    ros::Subscriber sub = nh->subscribe("canTx", 20, subCB);
+    ros::Subscriber sub = nh->subscribe("/cmd_vel", 20, cmd_cb);
     ros::AsyncSpinner spinner(1);
     std::string path;
     nh->getParam("path", path);
