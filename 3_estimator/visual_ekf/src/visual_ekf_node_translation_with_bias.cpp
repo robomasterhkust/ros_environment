68ffcac22b0b290fef7b199744020a277ffb0cf0
Beck Ubuntu VM <pshfls@gmail.com>
2018-07-08 22:18:00 +0800

    15 states is back. The unstable datafeed really make the EKF unstable. Need to test on a stable datafeed and use seperate update for two estimate

--- 3_estimator/visual_ekf/src/visual_ekf_node_translation_with_bias.cpp
+++ 3_estimator/visual_ekf/src/visual_ekf_node_translation_with_bias.cpp
@@ -28,7 +28,7 @@ ros::Publisher pose_pub, debug_pub, odom_pub, debug_propagate_pub;
 string imu_topic, pose_topic, visual_topic;
 string debug_topic, publisher_topic;
 double acc_weight, acc_bias_weight;
-double visual_q_weight, visual_t_weight;
+double gravity_q_weight, visual_t_weight;
 int sleep_time;
 
 /**
@@ -39,16 +39,16 @@ int sleep_time;
  * Define noises:
  *      n = [n_acc, n_gyro, n_bias_acc, n_bias_gyro]
  */
-VectorXd x(9);         // state
-MatrixXd P = MatrixXd::Identity(9, 9); // covariance
-MatrixXd R = MatrixXd::Identity(6, 6); // prediction noise covariance
-MatrixXd Q = MatrixXd::Identity(3, 3); // observation noise covariance
+VectorXd x(16);         // state
+MatrixXd P = MatrixXd::Identity(15, 15); // covariance
+MatrixXd R = MatrixXd::Identity(12, 12); // prediction noise covariance
+MatrixXd Q = MatrixXd::Identity(6, 6); // observation noise covariance
 
 // buffers to save gyro and visual reading
 queue<sensor_msgs::Imu::ConstPtr> imu_buf;
 queue<geometry_msgs::TwistStamped::ConstPtr> visual_buf;
-queue<Matrix<double, 9, 1>> x_history;
-queue<Matrix<double, 9, 9>> P_history;
+queue<Matrix<double, 16, 1>> x_history;
+queue<Matrix<double, 15, 15>> P_history;
 Vector3d G = {0, 0, 9.8}; // Consider to add initialization later
 
 double t_prev;          // previous propagated time
@@ -75,12 +75,17 @@ void pub_shield_odom(const std_msgs::Header& header)
     nav_msgs::Odometry odom;
     odom.header = header;
     odom.child_frame_id = "world";
-    odom.pose.pose.position.x = x(0);
-    odom.pose.pose.position.y = x(1);
-    odom.pose.pose.position.z = x(2);
-    odom.twist.twist.linear.x = x(3);
-    odom.twist.twist.linear.y = x(4);
-    odom.twist.twist.linear.z = x(5);
+    
+    odom.pose.pose.orientation.w = x(0);
+    odom.pose.pose.orientation.x = x(1);
+    odom.pose.pose.orientation.y = x(2);
+    odom.pose.pose.orientation.z = x(3);
+    odom.pose.pose.position.x = x(4);
+    odom.pose.pose.position.y = x(5);
+    odom.pose.pose.position.z = x(6);
+    odom.twist.twist.linear.x = x(7);
+    odom.twist.twist.linear.y = x(8);
+    odom.twist.twist.linear.z = x(9);
 
     odom.pose.covariance[0]  = P(0, 0);
     odom.pose.covariance[7]  = P(1, 1);
@@ -151,43 +156,68 @@ void pub_debug_propagate(const std_msgs::Header& header,
 void propagate(const sensor_msgs::Imu &imu)
 {
     double cur_t = imu.header.stamp.toSec();
-    Vector3d a, acc_wo_g;
+    Vector3d a, a_x, acc_wo_g, w, w_x;
     a(0) = imu.linear_acceleration.x;
     a(1) = imu.linear_acceleration.y;
     a(2) = imu.linear_acceleration.z;
-    Quaterniond world_R_imu(imu.orientation.w, imu.orientation.x, imu.orientation.y, imu.orientation.z);
+    w(0) = imu.angular_velocity.x;
+    w(1) = imu.angular_velocity.y;
+    w(2) = imu.angular_velocity.z;
+    a_x = a - x.segment<3>(10);
+    w_x = w - x.segment<3>(13);
+    Quaterniond world_R_imu(x(0), x(1), x(2), x(3));
 
     double dt = cur_t - t_prev;
     dt = (dt < IMU_UPDATE_TIME * 5) ? dt : IMU_UPDATE_TIME * 5;
     ROS_INFO("dt in propagate is %f, at the cur_t %f", dt, cur_t);
     // acc_wo_g = world_R_imu.toRotationMatrix() * (a - x.segment<3>(6)) - G;
-    acc_wo_g = (a - x.segment<3>(6)) - world_R_imu.toRotationMatrix().transpose() * G;
-    x.segment<3>(0) += x.segment<3>(3) * dt + 0.5 * acc_wo_g * dt * dt;
-    x.segment<3>(3) += acc_wo_g * dt;
+    acc_wo_g = a_x - world_R_imu.toRotationMatrix().transpose() * G;
+    x.segment<3>(4) += x.segment<3>(7) * dt + 0.5 * acc_wo_g * dt * dt;
+    x.segment<3>(7) += acc_wo_g * dt;
 
     a_int_int += a_int * dt + 0.5 * acc_wo_g * dt * dt;
     a_int += acc_wo_g * dt;
-    pub_debug_propagate(imu.header, acc_wo_g, x.segment<3>(6));
+    pub_debug_propagate(imu.header, acc_wo_g, a_int);
 
-    MatrixXd A = MatrixXd::Zero(9, 9);
-    A.block<3, 3>(0, 3) = MatrixXd::Identity(3, 3);
-    A.block<3, 3>(3, 6) = -world_R_imu.toRotationMatrix();
+    Vector3d domg = w_x * dt / 2;
+    Quaterniond dR(sqrt(1 - domg.squaredNorm()), domg(0), domg(1), domg(2));
+    Quaterniond R_now;
+    R_now = (world_R_imu * dR).normalized();
+    x.segment<4>(0) << R_now.w(), R_now.x(), R_now.y(), R_now.z();
 
-    MatrixXd U = -MatrixXd::Zero(9, 6);
-    U.block<3, 3>(3, 0) = -world_R_imu.toRotationMatrix();
-    U.block<3, 3>(6, 3) = MatrixXd::Identity(3, 3);
+
+    Matrix3d R_w_x, R_a_x;
+    R_w_x << 0, -w_x(2),  w_x(1),
+             w_x(2),  0, -w_x(0),
+            -w_x(1), w_x(0), 0;
+    R_a_x << 0, -a_x(2),  a_x(1),
+             a_x(2),  0, -a_x(0),
+            -a_x(1), a_x(0), 0;
+
+    MatrixXd A = MatrixXd::Zero(15, 15);
+    A.block<3, 3>(0, 0) = -R_w_x;
+    A.block<3, 3>(0, 12)= -MatrixXd::Identity(3, 3);
+    A.block<3, 3>(3, 6) =  MatrixXd::Identity(3, 3);
+    A.block<3, 3>(6, 0) = -R_a_x;
+    A.block<3, 3>(6, 9) = -world_R_imu.toRotationMatrix();
+
+    MatrixXd U = -MatrixXd::Zero(15, 12);
+    U.block<3, 3>(0, 0) = -MatrixXd::Identity(3, 3);
+    U.block<3, 3>(6, 3) = -world_R_imu.toRotationMatrix();
+    U.block<3, 3>(9, 6) =  MatrixXd::Identity(3, 3);
+    U.block<3, 3>(12,9) =  MatrixXd::Identity(3, 3);
 
     MatrixXd F, V;
-    F = MatrixXd::Identity(9, 9) + dt * A;
+    F = MatrixXd::Identity(15, 15) + dt * A;
     V = dt * U;
-    cout << "F " << endl << F << endl;
-    cout << "R " << endl << R << endl;
-    cout << "V " << endl << V << endl;
+//    cout << "F " << endl << F << endl;
+//    cout << "R " << endl << R << endl;
+//    cout << "V " << endl << V << endl;
     P = F * P * F.transpose() + V * R * V.transpose();
 	
     t_prev = cur_t;
-    cout << "P " << endl << P << endl;
-    cout << "x " << endl << x.transpose() << endl;
+//    cout << "P " << endl << P << endl;
+//    cout << "x " << endl << x.transpose() << endl;
 }
 
 static void throwState(double current_time)
@@ -254,21 +284,41 @@ static void update(const geometry_msgs::TwistStamped &pnp)
     // pub_debug_update(pnp.header, world_T_shield, world_R_imu, cur_t);
     pub_debug_update(pnp.header, imu_T_shield, world_R_imu, cur_t);
 
-    MatrixXd C = MatrixXd::Zero(3, 9);
+    MatrixXd C = MatrixXd::Zero(6, 15);
     C.block<3, 3>(0, 0) = MatrixXd::Identity(3, 3);
+    C.block<3, 3>(3, 3) = MatrixXd::Identity(3, 3);
 
-    Matrix3d W = MatrixXd::Identity(3, 3);
+    // Matrix3d W = MatrixXd::Identity(6, 6);
 
-    MatrixXd K(9, 3);
-    K = P * C.transpose() * (C * P * C.transpose() + W * Q * W.transpose()).inverse();
-    cout << "C " << endl << C << endl;
-    cout << "K " << endl << K << endl;
-    cout << "Q " << endl << Q << endl;
+    MatrixXd K(15, 6);
+    K = P * C.transpose() * (C * P * C.transpose() + Q).inverse();
+//    cout << "C " << endl << C << endl;
+//    cout << "K " << endl << K << endl;
+//    cout << "Q " << endl << Q << endl;
     // x = x + K * (world_T_shield - C * x);
-    x = x + K * (imu_T_shield - C * x);
+    // x = x + K * (imu_T_shield - C * x);
+
+    VectorXd r(6);
+    // Quaterniond qm(world_R_imu);
+    Quaterniond q  = Quaterniond(x(0), x(1), x(2), x(3));
+    Quaterniond dq = q.conjugate() * world_R_imu;
+    r.head<3>() = 2 * dq.vec();
+    r.tail<3>() = imu_T_shield - x.segment<3>(4);
+    VectorXd _r = K * r;
+    Vector3d dw(0.5 * _r(0), 0.5 * _r(1), 0.5 * _r(2));
+    dq = Quaterniond(1, dw(0), dw(1), dw(2)).normalized();
+    q = q * dq;
+
+    x(0) = q.w();
+    x(1) = q.x();
+    x(2) = q.y();
+    x(3) = q.z();
+
+    x.segment<12>(4) += _r.tail(12);
+
     P = P - K * C * P;
-    cout << "P " << endl << P << endl;
-    cout << "x " << endl << x.transpose() << endl;
+//    cout << "P " << endl << P << endl;
+//    cout << "x " << endl << x.transpose() << endl;
 
     if (imu_buf.size() > 1) {
         // repropagate();
@@ -325,9 +375,9 @@ static void initialize_visual(const geometry_msgs::TwistStamped::ConstPtr &pnp)
     pub_debug_update(pnp->header, imu_T_shield, world_R_imu, cur_t);
 
     x.setZero();
-    x.segment<3>(0) = imu_T_shield;
-    x.segment<3>(3) << 0, 0, 0;
-    x.segment<3>(6) << 0, 0, 0;
+    x.segment<4>(0) << world_R_imu.w(), world_R_imu.x(), world_R_imu.y(), world_R_imu.z();
+    x.segment<3>(4) << imu_T_shield(0), imu_T_shield(1), imu_T_shield(2);
+
 
     cout << "DEBUG: x initialized with " << endl << x.transpose() << endl;
 
@@ -391,15 +441,17 @@ int main(int argc, char **argv)
     n.param("debug_topic", debug_topic, string("/visual_ekf/debug_odom"));
     n.param("accelerometer_noise_weight", acc_weight, 1000.0);
     n.param("accelerometer_bias_noise_weight", acc_bias_weight, 100.0);
-    n.param("visual_pose_weight", visual_q_weight, 10.0);
+    n.param("gravity_pose_weight", gravity_q_weight, 10.0);
+    n.param("visual_translation_weight", visual_t_weight, 10.0);
     n.param("node_sleep_time", sleep_time, 0);
 
     ros::Duration(sleep_time).sleep();
 
     // TODO: initalize the R and Q matrix
-    R.topLeftCorner(3, 3)     = acc_weight * MatrixXd::Identity(3, 3); // accelerometer noise
-    R.bottomRightCorner(3, 3) = acc_bias_weight * MatrixXd::Identity(3, 3); // accelerometer noise
-    Q = visual_q_weight * MatrixXd::Identity(3, 3); // observation noise
+    R.topLeftCorner(6, 6)     = acc_weight * MatrixXd::Identity(6, 6); // gyro, accelerometer noise
+    R.bottomRightCorner(6, 6) = acc_bias_weight * MatrixXd::Identity(6, 6); // accelerometer bias, gyro bias noise
+    Q.topLeftCorner(3, 3)     = gravity_q_weight * MatrixXd::Identity(3, 3); // orientation noise
+    Q.bottomRightCorner(3, 3) = visual_t_weight * MatrixXd::Identity(3, 3); // position noise
 
     x.setZero();
 
@@ -421,10 +473,20 @@ int main(int argc, char **argv)
 }
 
 /**
- *  0   T_x     translation of the shield in world frame
- *  1   T_y
- *  2   T_z
- *  3   v_x     velocity of the shield in world frame
- *  4   v_y
- *  5   v_z
+ *  0   q_w     rotation from imu to world
+ *  1   q_x
+ *  2   q_y
+ *  3   q_z
+ *  4   T_x     translation of the shield in imu frame
+ *  5   T_y
+ *  6   T_z
+ *  7   v_x     velocity of the shield in imu frame
+ *  8   v_y
+ *  9   v_z
+ *  10  ba_x    bias in accelerometer
+ *  11  ba_y
+ *  12  ba_z
+ *  13  bw_x    bias in gyroscope
+ *  14  bw_y
+ *  15  bw_z
  */
