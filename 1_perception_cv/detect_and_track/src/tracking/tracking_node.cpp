92645852911ea2438a220eda94acc930b9e8dce6
Beck <pshfls@gmail.com>
2018-11-17 23:01:31 +0800

    change the state machine to better form, pass compile time

--- 1_perception_cv/detect_and_track/src/tracking/tracking_node.cpp
+++ 1_perception_cv/detect_and_track/src/tracking/tracking_node.cpp
@@ -4,6 +4,7 @@
  * @date 2018-11-17
  */
 #include <iostream>
+#include <vector>
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/tracking.hpp>
@@ -25,21 +26,52 @@ using namespace std;
 ros::Publisher bbox_pub;
 string cv_topic;
 string bbox_topic;
-double tracking_freq = 100;
+double tracking_freq = 80;
 double detection_freq = 30;
 double redetection_freq = 1;
 
-bool tracking_lost = false;
-bool armor_detected = false;
-bool detection_verified = false;
+bool image_received = false;
+
 Rect2d bbox_detect;
 Rect2d bbox_detect_prev;
-Rect2d bbox_track;
 
-unsigned int tracking_count = 0;
+enum _detect_state_t { IDLE, DETECTION, ASSOCIATION, TRACKING };
+_detect_state_t state = IDLE;
 
-enum _state_detect_and_track_t { NOT_DETECTED, DETECTION, TRACKING };
-_state_detect_and_track_t state = NOT_DETECTED;
+// image tracker
+vector<Mat> image_stack;
+Ptr<Tracker> tracker;
+
+bool
+detect(const Mat &cur_frame, Rect2d &bbox) {
+    return false;
+}
+
+void
+kalman_filter_init(const Rect2d &bbox) {
+
+}
+
+bool
+kalman_filter_update(const Rect2d &bbox) {
+    return false;
+}
+
+void
+kalman_filter_clear() {
+
+}
+
+void
+init_tracker(const Mat &cur_frame, const Rect2d &bbox) {
+    tracker = TrackerKCF::create();
+    tracker->init(cur_frame, bbox);
+}
+
+/* TODO: Add 2D feature correspondence
+bool
+feature_correspondence
+ */
 
 void
 cv_callback(const sensor_msgs::Image::ConstPtr image_ptr) {
@@ -59,52 +91,132 @@ int main(int argc, char **argv) {
     ros::Rate rate_track(tracking_freq);
     ros::Rate rate_detect(detection_freq);
 
+    // For state machine
+    unsigned int verify_count = 0;
+    unsigned int tracking_count = 0;
+    unsigned int verify_count_max = 5;
     unsigned int tracking_count_max = (unsigned int)(tracking_freq / redetection_freq);
 
     while (ros::ok()) {
         // state machine
         switch (state) {
-            case NOT_DETECTED:
-                state = armor_detected ? DETECTION : NOT_DETECTED;
+            case IDLE: {
+                state = image_received ? DETECTION : IDLE;
                 break;
-
-            case DETECTION:
-                if (!armor_detected) { state = NOT_DETECTED; }
-                else if (!detection_verified) { state = DETECTION;}
-                else { state = TRACKING;}
-                break;
-
-            case TRACKING:
-                if (tracking_lost) {
-                    state = NOT_DETECTED;
-                    tracking_count = 0;
+            }
+            case DETECTION: {
+                bool armor_detected = false;
+                // Action
+                if (image_received) {
+                    armor_detected = detect(image_stack.front(), bbox_detect);
                 }
-                else if (tracking_count >= tracking_count_max) {
+
+                // Change state condition
+                if (!image_received)
+                    state = IDLE;
+                else if (!armor_detected)
                     state = DETECTION;
-                    tracking_count = 0;
-                }
-                else {
-                    state = TRACKING;
-                    tracking_count++;
+                else
+                    state = ASSOCIATION;
+
+                // Describe the state edge
+                switch (state) {
+                    case IDLE:          break;
+                    case DETECTION:     break;
+                    case ASSOCIATION:
+                        verify_count = 0;
+                        kalman_filter_init(bbox_detect);
+                        break;
+                    case TRACKING:      break;
                 }
                 break;
-            default: break;
+            }
+            case ASSOCIATION: {
+                bool armor_detected = false;
+                bool detection_verified = false;
+                bool count_pass = false;
+
+                // Action
+                if (image_received) {
+                    armor_detected = detect(image_stack.front(), bbox_detect);
+                    if (armor_detected) {
+                        detection_verified = kalman_filter_update(bbox_detect);
+                        if (detection_verified) {
+                            verify_count++;
+                            count_pass = !(verify_count < verify_count_max);
+                        }
+                    }
+                }
+
+                // Change state condition
+                if (!image_received)
+                    state = IDLE;
+                else if (!armor_detected || !detection_verified )
+                    state = DETECTION;
+                else if (!count_pass)
+                    state = ASSOCIATION;
+                else
+                    state = TRACKING;
+
+                // Describe the state edge
+                switch (state) {
+                    case IDLE:          kalman_filter_clear(); break;
+                    case DETECTION:     kalman_filter_clear(); break;
+                    case ASSOCIATION:   break;
+                    case TRACKING:
+                        tracking_count = 0;
+                        init_tracker(image_stack.front(), bbox_detect);
+                        break;
+                }
+                break;
+            }
+            case TRACKING: {
+                bool tracking_ok = false;
+                bool count_pass = false;
+
+                // Action
+                if (image_received) {
+                    tracking_ok = tracker->update(image_stack.front(), bbox_detect);
+                    if (tracking_ok) {
+                        tracking_count++;
+                        count_pass = !(tracking_count < tracking_count_max);
+                    }
+                }
+
+                // Change state condition
+                if (!image_received)
+                    state = IDLE;
+                else if (!tracking_ok)
+                    state = DETECTION;
+                else if (count_pass)
+                    state = ASSOCIATION;
+                else
+                    state = TRACKING;
+
+                switch (state) {
+                    case IDLE:          break;
+                    case DETECTION:     break;
+                    case ASSOCIATION:   break;
+                    case TRACKING:
+                        verify_count = 0;
+                        kalman_filter_init(bbox_detect);
+                        break;
+                }
+                break;
+            }
         }
 
-        if (state == NOT_DETECTED) {
-            // bbox_detect = detect();
-            rate_detect.sleep();
+        // control update rate
+        switch (state) {
+            case IDLE:
+            case DETECTION:
+            case ASSOCIATION:
+                rate_detect.sleep();
+                break;
+            case TRACKING:
+                rate_track.sleep();
+                break;
         }
-        else if (state == DETECTION) {
-            // bbox_detect = detect();
-            // detection_verified = data_associate(bbox_detect_prev, bbox_detect);
-            rate_detect.sleep();
-        }
-        else if (state == TRACKING) {
-            // bbox_track = track();
-            rate_track.sleep();
-        }
-
         ros::spinOnce();
     }
 }
